4213a1c73db04f58317bc8d960645a3620c82661
acharyaprach01 <prachi.a@simformsolutions.com>
2022-06-13 16:04:41 +0530

    aws

--- first_djangoapp/settings.py
+++ first_djangoapp/settings.py
@@ -1,7 +1,7 @@
 import os
 from datetime import timedelta
 from pickle import TRUE
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -178,8 +178,8 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR,'static')
 # ]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-# django_heroku.settings(locals())
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+django_heroku.settings(locals())
 
 
 
